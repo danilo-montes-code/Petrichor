f24d1b2755943b85d230acd5bfaf9f27f91dae37
danilo-montes <dem9855@nyu.edu>
2023-04-18 14:51:58 -0400

    added keep alive file, finished bot feature

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Petrichor
 
 Petrichor is a mutlipurpose Discord Bot.
+
+## Functions
+* send messages from archive server to friend server channel
+
+*Tutorial referenced: https://docs.replit.com/tutorials/python/build-basic-discord-bot-python*
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import discord, os
 from dotenv import load_dotenv
-
+from keep_alive import keep_alive
+from discord import Message
 load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
+
 @client.event
-async def on_ready():
+async def on_ready() -> None:
     print(f'User {client.user} logged in')
 
 @client.event
-async def on_message(message):
+async def on_message(message: Message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if (message.guild.id == int(os.getenv('FANTA_ID')) and 
+       'clips' in message.channel.name):
+        content = message.content
+        power = client.get_channel(int(os.getenv('POWER_ID')))
+        await power.send(content=content)
 
+
+keep_alive()
 client.run(os.getenv('BOT_TOKEN'))
