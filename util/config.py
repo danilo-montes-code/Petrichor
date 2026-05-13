a0d209d576031a46704a3755a76628fc765d6eb2
Danilo Montes <dem9855@nyu.edu>
2026-05-13 00:59:54 -0400

    Add centralized vc euoh Literal

---
+++ util/config.py
@@ -0,0 +1,16 @@
+"""config.py
+
+Holds constants and other configuration variables for the bot.
+"""
+
+from typing import Literal
+
+
+VC_EUOH_TYPES = Literal[
+    'scuzz',
+    'kaeley',
+    'declan',
+    'armando',
+    'max moment',
+    'max yapment' # cSpell:disable-line
+]
